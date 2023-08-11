ba3db6cbaaaab71e5cdcbad41fe4f363e3fbe2a0
dayvedev <dayvedev@gmail.com>
2023-08-11 16:18:02 +0000

    Update SettingsDataStore.kt

--- app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/wordsapp/data/SettingsDataStore.kt
@@ -1,4 +1,59 @@
 package com.example.wordsapp.data
 
-class SettingsDataStore {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+
+
+private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
+
+// Create a DataStore instance using the preferencesDataStore delegate, with the Context as
+// receiver.
+/**
+ * Create the DataStore instance at the top level of your Kotlin file once, and access it through this property
+ * throughout the rest of your application. This makes it easier to keep your DataStore as a singleton.
+ */
+private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
+    name = LAYOUT_PREFERENCES_NAME
+)
+
+class SettingsDataStore(context: Context) {
+
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+    suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
+
+
+    /**
+     * SharedPreference DataStore throws an IOException when an error is encountered while reading data.
+     * In preferenceFlow declaration, before map(), use the catch() operator to catch the IOException and emit
+     * emptyPreferences()
+     */
+    val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                //if a different type of exception is thrown, re-throw it.
+                throw it
+            }
+        }
+        .map { preferences ->
+            // On the first run of the app, we will use LinearLayoutManager by default
+            preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
+
 }
